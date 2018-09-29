7a1c15e1f3b15171886bc2374d811f91afa781fe
Richard Guan <guanr@umich.edu>
2018-09-29 17:09:14 -0400

    Made vision api work in client (probably) by adding a vision Client

--- image_rec.py
+++ image_rec.py
@@ -1,5 +1,8 @@
 class Flier(object):
     def __init__(image):
+        from google.cloud import vision
+        from google.cloud.vision import types
+        vision_client = vision.Client()
         self.image = image
 
     def ocr():
